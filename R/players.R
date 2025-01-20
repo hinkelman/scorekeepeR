b4712bdbdac7132dc548c812aff57a343c06fa2b
Travis Hinkelman <thinkelman@pm.me>
2025-01-19 20:48:05 -0800

    Delete edit_players_row; change init_players and add_players

--- R/players.R
+++ R/players.R
@@ -9,9 +9,9 @@
 #'
 
 init_players_table <- function(){
-  data.frame(PlayerID = ids::random_id(),
-             FirstName = NA_character_,
-             LastName = NA_character_)
+  data.frame(PlayerID = character(),
+             FirstName = character(),
+             LastName = character())
 }
 
 
@@ -26,26 +26,9 @@ init_players_table <- function(){
 #'
 
 add_players_row = function(players_table){
-  rbind(players_table, init_players_table())
+  rbind(players_table,
+        data.frame(PlayerID = ids::random_id(),
+                   FirstName = "",
+                   LastName = ""))
 }
 
-
-#' Edit players row
-#'
-#' Edit row in players table
-#'
-#' @md
-#' @param players_table Players table
-#' @param row           Row index of cell to update
-#' @param column        Column index of cell to update
-#' @param value         Value to place in cell
-#'
-#' @export
-#'
-
-edit_players_row = function(players_table, row, col, value){
-  if (col == 1) stop("PlayerID column (col = 1) can't be updated")
-  if (!is.character(value)) value = as.character(value)
-  players_table[row, col] = value
-  players_table
-}
