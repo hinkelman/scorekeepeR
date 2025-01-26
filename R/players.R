09e3323690de8d99c66045dab9bb049505632ebc
Travis Hinkelman <thinkelman@pm.me>
2025-01-25 16:55:29 -0800

    Change approach to create_player_names and update tests

--- R/players.R
+++ R/players.R
@@ -37,36 +37,28 @@ add_players_row <- function(players_table){
 #' Create player names for display in app
 #'
 #' @md
-#' @param first_names    Vector of players' first names
-#' @param last_names     Vector of players' last names
-#' @param numbers        Vector of players' numbers
-#' @param tab            Tab where name will be displayed: roster or scorekeeper
+#' @param data    Dataframe that includes columns for FirstName, LastName, and Number
 #'
 #' @export
 #'
 
-create_player_names <- function(first_names, last_names, numbers,
-                                tab = c("roster", "scorekeeper")){
-
-  out = data.frame(FirstName = first_names,
-                   LastName = last_names,
-                   Number = numbers) |>
+create_player_names <- function(data){
+  data |>
     dplyr::mutate(
-      roster = dplyr::case_when(
+      Name = dplyr::case_when(
         is.na(FirstName) & is.na(LastName) ~ NA_character_,
         is.na(FirstName) ~ LastName,
         is.na(LastName) ~ FirstName,
         .default = paste(FirstName, LastName)),
-      Name = dplyr::case_when(
+      name_tmp = dplyr::case_when(
         is.na(FirstName) & is.na(LastName) ~ NA_character_,
         is.na(FirstName) ~ LastName,
         .default = FirstName),
-      scorekeeper = dplyr::case_when(
-        is.na(Name) & is.na(Number) ~ NA_character_,
-        is.na(Number) ~ Name,
-        is.na(Name) ~ paste0("#", Number),
-        .default = paste0(Name, " (#", Number, ")")))
-
-  out[[tab]][!is.na(out[[tab]])]
+      NameNum = dplyr::case_when(
+        is.na(name_tmp) & is.na(Number) ~ NA_character_,
+        is.na(Number) ~ name_tmp,
+        is.na(name_tmp) ~ paste0("#", Number),
+        .default = paste0(name_tmp, " (#", Number, ")"))) |>
+    dplyr::select(-name_tmp)
 }
 
--- tests/testthat/test-players.R
+++ tests/testthat/test-players.R
@@ -1,22 +1,20 @@
-players_table = data.frame(PlayerID = as.character(1:3),
-                           FirstName = c("Travis", "Jared", "Owen"),
-                           LastName = "Hinkelman")
 
+players_table = data.frame(PlayerID = c("1", "2"),
+                           FirstName = c("Travis", "Jared"),
+                           LastName = c(NA, NA))
 pt2 = add_players_row(players_table)
 
+tmp = data.frame(FirstName = c(NA, "Steph", NA, "Steph", "Steph"),
+                 LastName = c("Curry", NA, NA, "Curry", "Curry"),
+                 Number = c(NA, NA, "32", NA, "32"))
+
 test_that("players", {
-  expect_equal(nrow(pt2), 4)
-  expect_equal(pt2$FirstName[4], NA_character_)
-  expect_length(create_player_names(NA, NA, NA, "roster"), 0)
-  expect_length(create_player_names(NA, NA, "32", "roster"), 0)
-  expect_length(create_player_names(NA, NA, NA, "scorekeeper"), 0)
-  expect_equal(create_player_names(NA, "Curry", NA, "roster"), "Curry")
-  expect_equal(create_player_names("Steph", NA, NA, "roster"), "Steph")
-  expect_equal(create_player_names("Steph", "Curry", NA, "roster"), "Steph Curry")
-  expect_equal(create_player_names("Steph", "Curry", "32", "roster"), "Steph Curry")
-  expect_equal(create_player_names(NA, NA, "32", "scorekeeper"), "#32")
-  expect_equal(create_player_names(NA, "Curry", NA, "scorekeeper"), "Curry")
-  expect_equal(create_player_names("Steph", NA, NA, "scorekeeper"), "Steph")
-  expect_equal(create_player_names("Steph", "Curry", NA, "scorekeeper"), "Steph")
-  expect_equal(create_player_names("Steph", "Curry", "32", "scorekeeper"), "Steph (#32)")
+  expect_equal(nrow(pt2), 3)
+  expect_equal(pt2$FirstName[3], NA_character_)
+  expect_true(all.equal(create_player_names(tmp),
+                        cbind(tmp,
+                              data.frame(Name = c("Curry", "Steph", NA,
+                                                  "Steph Curry", "Steph Curry"),
+                                         NameNum = c("Curry", "Steph", "#32",
+                                                     "Steph", "Steph (#32)")))))
 })
