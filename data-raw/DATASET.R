efec5d270c92a57ed33cbf363a51aedae2a4816e
Travis Hinkelman <thinkelman@pm.me>
2025-01-27 22:29:37 -0800

    Change order of events and events_desc

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,11 +1,11 @@
 
-events = c("FTA", "FTM", "FGA2", "FGM2", "FGA3", "FGM3",
-           "STL", "TOL", "DREB", "OREB", "BLK", "AST", "PF")
+events = c("FTM", "FTA", "FGM2", "FGA2", "FGM3", "FGA3",
+           "STL", "TOV", "DREB", "OREB", "BLK", "AST", "PF")
 usethis::use_data(events, overwrite = TRUE)
 
-events_desc = c("Missed free throw", "Made free throw",
-                "Missed field goal", "Made field goal",
-                "Missed 3-pt field goal", "Made 3-pt field goal",
+events_desc = c("Made free throw", "Missed free throw",
+                "Made field goal", "Missed field goal",
+                "Made 3-pt field goal", "Missed 3-pt field goal",
                 "Steal", "Turnover", "Defensive Rebound",
                 "Offensive Rebound", "Block", "Assist", "Foul")
 usethis::use_data(events_desc, overwrite = TRUE)
