9511d5519f1ab5e2452ad089a2ce9368c2110546
Travis Hinkelman <thinkelman@pm.me>
2025-02-06 11:35:49 -0800

    Change parameter order for add_log_entry

--- R/game_logs.R
+++ R/game_logs.R
@@ -24,14 +24,14 @@ create_log_header <- function(team_id, game_id, date, opponent){
 #'
 #' @md
 #' @param game_log   Game log comprised of vector of event descriptions
+#' @param player     Player display name
 #' @param event      Game event (e.g., FGM, STL, etc.)
-#' @param player     Player name
 #' @param undo       Boolean indicating if event was undone
 #'
 #' @export
 #'
 
-add_log_entry <- function(game_log, event = events, player, undo = FALSE){
+add_log_entry <- function(game_log, player, event = events, undo = FALSE){
   if (!is.logical(undo)) stop("undo must be TRUE or FALSE")
   event = match.arg(event)
   undo_txt = if (undo) "UNDO " else ""
