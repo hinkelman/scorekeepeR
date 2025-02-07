a78248c7941983fabeeed98e0bb205d888094006
Travis Hinkelman <thinkelman@pm.me>
2025-02-07 07:34:19 -0800

    Update game log tests

--- tests/testthat/test-game_logs.R
+++ tests/testthat/test-game_logs.R
@@ -1,9 +1,9 @@
 
-test_that("create_log_entry", {
-  expect_equal(create_log_entry(FALSE, "FGM3", "Stephen Curry"),
+test_that("add_log_entry", {
+  expect_equal(add_log_entry(NULL, "Stephen Curry", "FGM3", FALSE),
                "Made 3-pt field goal by Stephen Curry")
-  expect_equal(create_log_entry(TRUE, "FGM3", "Travis"),
+  expect_equal(add_log_entry(NULL, "Travis", "FGM3", TRUE),
                "UNDO Made 3-pt field goal by Travis")
-  expect_error(create_log_entry("false", "FTA", "Test"))
-  expect_error(create_log_entry(FALSE, "FG", "Test"))
+  expect_error(add_log_entry(NULL, "Test", "FGM3", "false"))
+  expect_error(add_log_entry(NULL, "Test", "FG", FALSE))
 })
