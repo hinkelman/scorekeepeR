8f2ebd849895a73bb1936c2ebe7d1da48257c37d
Travis Hinkelman <thinkelman@pm.me>
2025-04-08 21:35:15 -0700

    Add stats_display_cols to datasets

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -10,3 +10,14 @@ events_desc = c("Made free throw", "Missed free throw",
                 "Offensive Rebound", "Block", "Assist", "Foul")
 usethis::use_data(events_desc, overwrite = TRUE)
 
+# counting stats stored in GameStats.csv
+# points and rebounds are only columns that could be calculated from other columns
+stats_cols = c("FTM", "FTA", "FGM2", "FGA2", "FGM3", "FGA3", "TOV", "STL",
+               "DREB", "OREB", "BLK", "AST", "PF", "PTS", "REB", "DNP")
+
+# common columns used for statistics display
+stats_display_cols = c("PTS", "FGM", "FGA", "FG%", "3PM" = "FGM3", "3PA" = "FGA3", "3P%",
+                       "FTM", "FTA", "FT%", "TS%", "OREB", "DREB", "REB",
+                       "AST", "TOV", "STL", "BLK", "PF")
+usethis::use_data(stats_display_cols, overwrite = TRUE)
+
